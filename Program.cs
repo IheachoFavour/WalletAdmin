a035687aaf0e84fe50a6105728c716eaec1b3541
Favour Iheacho <fciheacho@gmail.com>
2026-01-13 12:16:48 +0100

    Fix Railway PostgreSQL connection parsing

--- Program.cs
+++ Program.cs
@@ -24,16 +24,20 @@ else
 {
     // RAILWAY / PRODUCTION
     var uri = new Uri(databaseUrl);
-    var userInfo = uri.UserInfo.Split(':');
+
+    var username = uri.UserInfo.Split(':')[0];
+    var password = uri.UserInfo.Split(':')[1];
 
     connectionString =
         $"Host={uri.Host};" +
         $"Port={uri.Port};" +
         $"Database={uri.AbsolutePath.TrimStart('/')};" +
-        $"Username={userInfo[0]};" +
-        $"Password={userInfo[1]};" +
-        $"SSL Mode=Require;Trust Server Certificate=true";
+        $"Username={username};" +
+        $"Password={password};" +
+        $"SslMode=Require;" +
+        $"Trust Server Certificate=true;";
 }
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
