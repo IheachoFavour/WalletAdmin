2b5654e5c3f15ed614ef1022239b8aa6bacfd91f
Favour Iheacho <fciheacho@gmail.com>
2026-01-13 13:46:44 +0100

    Program.cs update

--- Program.cs
+++ Program.cs
@@ -11,23 +11,31 @@ var builder = WebApplication.CreateBuilder(args);
     //options.UseNpgsql(connectionString));
 
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-Console.WriteLine("DATABASE_URL = " + databaseUrl);
 
 string connectionString;
-Console.WriteLine($"DATABASE_URL from env: {(databaseUrl ?? "NULL - NOT SET!!!")}");
 
-if (string.IsNullOrEmpty(databaseUrl))
+if (string.IsNullOrWhiteSpace(databaseUrl))
 {
     // LOCAL DEVELOPMENT
     connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        ?? throw new InvalidOperationException("DefaultConnection not found.");
 }
 else
 {
-    Console.WriteLine("Using Railway DATABASE_URL");
     // RAILWAY / PRODUCTION
-    connectionString = databaseUrl;
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+
+    connectionString =
+        $"Host={uri.Host};" +
+        $"Port={uri.Port};" +
+        $"Database={uri.AbsolutePath.TrimStart('/')};" +
+        $"Username={userInfo[0]};" +
+        $"Password={userInfo[1]};" +
+        $"SslMode=Require;" +
+        $"Trust Server Certificate=true;";
 }
+
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -53,18 +61,13 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
 }
-
-
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
